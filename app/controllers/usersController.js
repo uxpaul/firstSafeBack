b509d5d5fd4842a6ea4d4b88bc8ec9ef1111538f
uxpaul <hoedevproject@gmail.com>
2016-12-16 14:33:46 +0100

    fix auth

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -3,37 +3,13 @@
 let jwt = require('jsonwebtoken')
 let Controller = require('./Controller')
 const USER = require('../models/users')
+
+
 class usersController extends Controller {
     constructor(io) {
         super(USER)
       }
 
-      connect(req, res, next) {
-          if (!req.body.nom ||  !req.body.password) {
-              res.status(400).send("Please enter your email and password")
-          } else {
-              USER.findOne(req.body, {
-                  password: 0
-              }, (err, user) => {
-                  if (err)
-                      next(err)
-                  else if (!user)
-                      res.sendStatus(403)
-                  else {
-                      let token = jwt.sign(user, ENV.token, {
-                          expiresIn: "24h"
-                      })
-
-                      res.json({
-                          success: true,
-                          user: user,
-                          token: token
-                      })
-
-                  }
-              })
-          }
-      }
 
       findOne(req,res,next) {
         let username = req.params.username
