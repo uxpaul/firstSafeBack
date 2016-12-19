eaf92ba354ab09bb66b6b796e03b960b61aef64b
uxpaul <hoedevproject@gmail.com>
2016-12-19 12:42:31 +0100

    improve

--- app/controllers/AidProvidersController.js
+++ app/controllers/AidProvidersController.js
@@ -31,7 +31,7 @@ class AidProvidersController extends Controller {
                     numClients: numClients
                 });
 
-                nsp.emit('disconnect', socket.id)
+                nsp.emit('disconnect', {id :socket.id})
                     //    console.log('Connected clients:', numClients);
             });
 
